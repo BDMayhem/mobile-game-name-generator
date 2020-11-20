08b650683c90cabe7410e4d42ecbc4c55825703a
Brian <brianjdiaz@gmail.com>
2020-11-20 14:25:10 -0500

    Update data, control placement

--- app.js
+++ app.js
@@ -1,286 +1,858 @@
 let names = [
-    {
-        name: "War",
-        num: 62
-    },{
-        name: "Heroes",
-        num: 55
-    },{
-        name: "Dragon",
-        num: 34
-    },{
-        name: "Legend",
-        num: 31
-    },{
-        name: "Battle",
-        num: 28
-    },{
-        name: "Monster",
-        num: 23
-    },{
-        name: "Adventure",
-        num: 23
-    },{
-        name: "Mobile",
-        num: 23
-    },{
-        name: "RPG",
-        num: 22
-    },{
-        name: "World",
-        num: 22
-    },{
-        name: "Fantasy",
-        num: 19
-    },{
-        name: "Star",
-        num: 19
-    },{
-        name: "Quest",
-        num: 19
-    },{
-        name: "Kingdom",
-        num: 19
-    },{
-        name: "Knights",
-        num: 17
-    },{
-        name: "Warriors",
-        num: 17
-    },{
-        name: "Empire",
-        num: 17
-    },{
-        name: "Souls",
-        num: 16
-    },{
-        name: "Final",
-        num: 16
-    },{
-        name: "Alliance",
-        num: 16
-    },{
-        name: "Clash",
-        num: 14
-    },{
-        name: "Blade",
-        num: 14
-    },{
-        name: "Dungeon",
-        num: 14
-    },{
-        name: "King",
-        num: 12
-    },{
-        name: "Saga",
-        num: 12
-    },{
-        name: "Guardians",
-        num: 12
-    },{
-        name: "Conquest",
-        num: 12
-    },{
-        name: "Arena",
-        num: 11
-    },{
-        name: "Super",
-        num: 11
-    },{
-        name: "Gods",
-        num: 11
-    },{
-        name: "Age",
-        num: 11
-    },{
-        name: "Mini",
-        num: 11
-    },{
-        name: "Zombie",
-        num: 10
-    },{
-        name: "Blood",
-        num: 10
-    },{
-        name: "Raid",
-        num: 10
-    },{
-        name: "Puzzle",
-        num: 10
-    },{
-        name: "Tactics",
-        num: 10
-    },{
-        name: "Titan",
-        num: 10
-    },{
-        name: "Force",
-        num: 10
-    },{
-        name: "Fighters",
-        num: 10
-    },{
-        name: "Master",
-        num: 9
-    },{
-        name: "Online",
-        num: 9
-    },{
-        name: "Guns",
-        num: 9
-    },{
-        name: "League",
-        num: 9
-    },{
-        name: "Sword",
-        num: 9
-    },{
-        name: "City",
-        num: 9
-    },{
-        name: "Castle",
-        num: 9
-    },{
-        name: "Champions",
-        num: 8
-    },{
-        name: "Duel",
-        num: 8
-    },{
-        name: "Eternal",
-        num: 8
-    },{
-        name: "Lords",
-        num: 8
-    },{
-        name: "Spirit",
-        num: 8
-    },{
-        name: "Tap",
-        num: 8
-    },{
-        name: "Glory",
-        num: 8
-    },{
-        name: "Go",
-        num: 8
-    },{
-        name: "Magic",
-        num: 7
-    },{
-        name: "Pocket",
-        num: 7
-    },{
-        name: "Rival",
-        num: 7
-    },{
-        name: "Squad",
-        num: 7
-    },{
-        name: "Summoners",
-        num: 7
-    },{
-        name: "Turbo",
-        num: 7
-    },{
-        name: "Tales",
-        num: 6
-    },{
-        name: "Epic",
-        num: 6
-    },{
-        name: "Brave",
-        num: 6
-    },{
-        name: "Space",
-        num: 6
-    },{
-        name: "Storm",
-        num: 6
-    },{
-        name: "Racer",
-        num: 6
-    },{
-        name: "Fire",
-        num: 6
-    },{
-        name: "Little",
-        num: 5
-    },{
-        name: "Heart",
-        num: 5
-    },{
-        name: "Ultimate",
-        num: 5
-    },{
-        name: "Brave",
-        num: 5
-    },{
-        name: "Tiny",
-        num: 5
-    },{
-        name: "Ghost",
-        num: 5
-    },{
-        name: "Rune",
-        num: 5
-    },{
-        name: "Project",
-        num: 5
-    },{
-        name: "Soldiers",
-        num: 5
-    },{
-        name: "Mighty",
-        num: 5
-    },{
-        name: "Galaxy",
-        num: 5
-    },{
-        name: "Cosmic Warrior",
-        num: 5
-    },{
-        name: "Origins",
-        num: 5
-    },{
-        name: "Crash",
-        num: 5
-    }
+  {
+    name: "heroes",
+    num: 328
+  },
+  {
+    name: "of",
+    num: 150,
+    placement: "middle"
+  },
+  {
+    name: "of the",
+    num: 89,
+    placement: "middle"
+  },
+  {
+    name: "the",
+    num: 139,
+    placement: "first"
+  },
+  {
+    name: "war",
+    num: 151
+  },
+  {
+    name: "legend",
+    num: 139
+  },
+  {
+    name: "dragon",
+    num: 124
+  },
+  {
+    name: "battle",
+    num: 120
+  },
+  {
+    name: "kingdom",
+    num: 87
+  },
+  {
+    name: "3D",
+    num: 79
+  },
+  {
+    name: "RPG",
+    num: 74
+  },
+  {
+    name: "fantasy",
+    num: 73
+  },
+  {
+    name: "mobile",
+    num: 73
+  },
+  {
+    name: "knights",
+    num: 71
+  },
+  {
+    name: "monster",
+    num: 67
+  },
+  {
+    name: "soul",
+    num: 63
+  },
+  {
+    name: "quest",
+    num: 62
+  },
+  {
+    name: "clash",
+    num: 61
+  },
+  {
+    name: "hunter",
+    num: 58
+  },
+  {
+    name: "world",
+    num: 57
+  },
+  {
+    name: "warriors",
+    num: 56
+  },
+  {
+    name: "dungeon",
+    num: 55
+  },
+  {
+    name: "star",
+    num: 51
+  },
+  {
+    name: "gods",
+    num: 51
+  },
+  {
+    name: "guardians",
+    num: 51
+  },
+  {
+    name: "league",
+    num: 51
+  },
+  {
+    name: "adventure",
+    num: 50
+  },
+  {
+    name: "zombie",
+    num: 49
+  },
+  {
+    name: "game",
+    num: 48
+  },
+  {
+    name: "saga",
+    num: 48
+  },
+  {
+    name: "wars",
+    num: 48
+  },
+  {
+    name: "age",
+    num: 47
+  },
+  {
+    name: "blade",
+    num: 42
+  },
+  {
+    name: "mmorpg",
+    num: 42
+  },
+  {
+    name: "arena",
+    num: 41
+  },
+  {
+    name: "empire",
+    num: 41
+  },
+  {
+    name: "magic",
+    num: 41
+  },
+  {
+    name: "puzzle",
+    num: 39
+  },
+  {
+    name: "rush",
+    num: 39
+  },
+  {
+    name: "tales",
+    num: 39
+  },
+  {
+    name: "king",
+    num: 38
+  },
+  {
+    name: "tap",
+    num: 38
+  },
+  {
+    name: "final",
+    num: 37
+  },
+  {
+    name: "run",
+    num: 36
+  },
+  {
+    name: "city",
+    num: 35
+  },
+  {
+    name: "sword",
+    num: 35
+  },
+  {
+    name: "dark",
+    num: 34
+  },
+  {
+    name: "lords",
+    num: 34
+  },
+  {
+    name: "online",
+    num: 34
+  },
+  {
+    name: "pocket",
+    num: 33
+  },
+  {
+    name: "chaos",
+    num: 31
+  },
+  {
+    name: "fighter",
+    num: 31
+  },
+  {
+    name: "soccer",
+    num: 31
+  },
+  {
+    name: "strike",
+    num: 31
+  },
+  {
+    name: "story",
+    num: 30
+  },
+  {
+    name: "racing",
+    num: 30
+  },
+  {
+    name: "squad",
+    num: 30
+  },
+  {
+    name: "super",
+    num: 30
+  },
+  {
+    name: "chronicle",
+    num: 29
+  },
+  {
+    name: "champions",
+    num: 27
+  },
+  {
+    name: "force",
+    num: 27
+  },
+  {
+    name: "epic",
+    num: 26
+  },
+  {
+    name: "lost",
+    num: 26
+  },
+  {
+    name: "titans",
+    num: 26
+  },
+  {
+    name: "X",
+    num: 25
+  },
+  {
+    name: "legacy",
+    num: 25
+  },
+  {
+    name: "storm",
+    num: 25
+  },
+  {
+    name: "summoner",
+    num: 25
+  },
+  {
+    name: "tactics",
+    num: 25
+  },
+  {
+    name: "master",
+    num: 24
+  },
+  {
+    name: "moba",
+    num: 24
+  },
+  {
+    name: "blood",
+    num: 23
+  },
+  {
+    name: "last",
+    num: 23
+  },
+  {
+    name: "rise",
+    num: 23
+  },
+  {
+    name: "brave",
+    num: 22
+  },
+  {
+    name: "dawn",
+    num: 22
+  },
+  {
+    name: "dead",
+    num: 21
+  },
+  {
+    name: "football",
+    num: 21
+  },
+  {
+    name: "ultimate",
+    num: 21
+  },
+  {
+    name: "defense",
+    num: 20
+  },
+  {
+    name: "fight",
+    num: 20
+  },
+  {
+    name: "fire",
+    num: 20
+  },
+  {
+    name: "idle",
+    num: 20
+  },
+  {
+    name: "monsters",
+    num: 20
+  },
+  {
+    name: "shadow",
+    num: 20
+  },
+  {
+    name: "action",
+    num: 19
+  },
+  {
+    name: "simulator",
+    num: 19
+  },
+  {
+    name: "attack",
+    num: 18
+  },
+  {
+    name: "masters",
+    num: 18
+  },
+  {
+    name: "one",
+    num: 18
+  },
+  {
+    name: "day",
+    num: 17
+  },
+  {
+    name: "global",
+    num: 17
+  },
+  {
+    name: "glory",
+    num: 17
+  },
+  {
+    name: "light",
+    num: 17
+  },
+  {
+    name: "ninja",
+    num: 17
+  },
+  {
+    name: "survival",
+    num: 17
+  },
+  {
+    name: "manager",
+    num: 16
+  },
+  {
+    name: "raid",
+    num: 16
+  },
+  {
+    name: "rivals",
+    num: 16
+  },
+  {
+    name: "tiny",
+    num: 16
+  },
+  {
+    name: "vs.",
+    num: 16,
+    placement: "middle"
+  },
+  {
+    name: "castle",
+    num: 15
+  },
+  {
+    name: "clicker",
+    num: 15
+  },
+  {
+    name: "dash",
+    num: 15
+  },
+  {
+    name: "destiny",
+    num: 15
+  },
+  {
+    name: "dynasty",
+    num: 15
+  },
+  {
+    name: "island",
+    num: 15
+  },
+  {
+    name: "line",
+    num: 15
+  },
+  {
+    name: "time",
+    num: 15
+  },
+  {
+    name: "combat",
+    num: 14
+  },
+  {
+    name: "raiders",
+    num: 14
+  },
+  {
+    name: "origin",
+    num: 14
+  },
+  {
+    name: "shooter",
+    num: 14
+  },
+  {
+    name: "sniper",
+    num: 14
+  },
+  {
+    name: "throne",
+    num: 14
+  },
+  {
+    name: "town",
+    num: 14
+  },
+  {
+    name: "asphalt",
+    num: 13
+  },
+  {
+    name: "boom",
+    num: 13
+  },
+  {
+    name: "card",
+    num: 13
+  },
+  {
+    name: "dice",
+    num: 13
+  },
+  {
+    name: "fighting",
+    num: 13
+  },
+  {
+    name: "frontier",
+    num: 13
+  },
+  {
+    name: "galaxy",
+    num: 13
+  },
+  {
+    name: "hearts",
+    num: 13
+  },
+  {
+    name: "iron",
+    num: 13
+  },
+  {
+    name: "legendary",
+    num: 13
+  },
+  {
+    name: "mini",
+    num: 13
+  },
+  {
+    name: "sky",
+    num: 13
+  },
+  {
+    name: "smash",
+    num: 13
+  },
+  {
+    name: "team",
+    num: 13
+  },
+  {
+    name: "touch",
+    num: 13
+  },
+  {
+    name: "tower",
+    num: 13
+  },
+  {
+    name: "angry",
+    num: 12
+  },
+  {
+    name: "art",
+    num: 12
+  },
+  {
+    name: "assault",
+    num: 12
+  },
+  {
+    name: "blitz",
+    num: 12
+  },
+  {
+    name: "call",
+    num: 12
+  },
+  {
+    name: "conquest",
+    num: 12
+  },
+  {
+    name: "heart",
+    num: 12
+  },
+  {
+    name: "infinity",
+    num: 12
+  },
+  {
+    name: "kings",
+    num: 12
+  },
+  {
+    name: "nest",
+    num: 12
+  },
+  {
+    name: "power",
+    num: 12
+  },
+  {
+    name: "revenge",
+    num: 12
+  },
+  {
+    name: "zombies",
+    num: 12
+  },
+  {
+    name: "birds",
+    num: 11
+  },
+  {
+    name: "darkness",
+    num: 11
+  },
+  {
+    name: "dream",
+    num: 11
+  },
+  {
+    name: "eternal",
+    num: 11
+  },
+  {
+    name: "evolution",
+    num: 11
+  },
+  {
+    name: "fishing",
+    num: 11
+  },
+  {
+    name: "go",
+    num: 11
+  },
+  {
+    name: "man",
+    num: 11
+  },
+  {
+    name: "immortal",
+    num: 11
+  },
+  {
+    name: "infinite",
+    num: 11
+  },
+  {
+    name: "land",
+    num: 11
+  },
+  {
+    name: "paradise",
+    num: 11
+  },
+  {
+    name: "part",
+    num: 11
+  },
+  {
+    name: "pirate",
+    num: 11
+  },
+  {
+    name: "racer",
+    num: 11
+  },
+  {
+    name: "rage",
+    num: 11
+  },
+  {
+    name: "ragnarok",
+    num: 11
+  },
+  {
+    name: "realm",
+    num: 11
+  },
+  {
+    name: "road",
+    num: 11
+  },
+  {
+    name: "space",
+    num: 11
+  },
+  {
+    name: "tennis",
+    num: 11
+  },
+  {
+    name: "alliance",
+    num: 10
+  },
+  {
+    name: "ark",
+    num: 10
+  },
+  {
+    name: "craft",
+    num: 10
+  },
+  {
+    name: "devil",
+    num: 10
+  },
+  {
+    name: "earth",
+    num: 10
+  },
+  {
+    name: "era",
+    num: 10
+  },
+  {
+    name: "genesis",
+    num: 10
+  },
+  {
+    name: "legion",
+    num: 10
+  },
+  {
+    name: "mighty",
+    num: 10
+  },
+  {
+    name: "modern",
+    num: 10
+  },
+  {
+    name: "panda",
+    num: 10
+  },
+  {
+    name: "pixel",
+    num: 10
+  },
+  {
+    name: "race",
+    num: 10
+  },
+  {
+    name: "stickman",
+    num: 10
+  },
+  {
+    name: "stone",
+    num: 10
+  },
+  {
+    name: "street",
+    num: 10
+  },
+  {
+    name: "academy",
+    num: 9
+  },
+  {
+    name: "commander",
+    num: 9
+  },
+  {
+    name: "demon",
+    num: 9
+  },
+  {
+    name: "duel",
+    num: 9
+  },
+  {
+    name: "dungeons",
+    num: 9
+  },
+  {
+    name: "endless",
+    num: 9
+  },
+  {
+    name: "explore",
+    num: 9
+  },
+  {
+    name: "fast",
+    num: 9
+  },
+  {
+    name: "kill",
+    num: 9
+  },
+  {
+    name: "luna",
+    num: 9
+  },
+  {
+    name: "mania",
+    num: 9
+  },
+  {
+    name: "metal",
+    num: 9
+  },
+  {
+    name: "moon",
+    num: 9
+  },
+  {
+    name: "odyssey",
+    num: 9
+  },
+  {
+    name: "party",
+    num: 9
+  },
+  {
+    name: "revolution",
+    num: 9
+  },
+  {
+    name: "rising",
+    num: 9
+  },
+  {
+    name: "siege",
+    num: 9
+  }
 ];
+
 let used = [];
+let name = "";
 
-document.getElementById("button").addEventListener("click", chooseThree);
+document.getElementById("button").addEventListener("click", createTitle);
 
-function chooseThree(){
-    if (used.length >= 30){
-        used.forEach(function(e){
-            names.push(Object.assign(e));
-        });
-        used = [];
+function createTitle(){
+  name = ""
+  if (used.length >= 30){
+    used.forEach(function(e){
+      names.push(Object.assign(e));
+    });
+    used = [];
+  }
+
+  const numb = Math.floor(Math.random() * 3) + 2;
+
+  for (let i = 0; i < numb; i++){
+    let sum = 0;
+    names.forEach(function(e){
+      sum += e.num;
+    });
+
+    chooseName(sum, i, numb)
+  }
+  document.getElementById("show").innerText = name;
+}
+
+function chooseName(sum, i, numb) {
+  let rand = Math.floor(Math.random() * sum);
+
+  for (let j = 0; j < names.length; j++){
+    rand -= names[j].num;
+    if (rand <= 0){
+
+      if (i > 0 && names[j].placement === "first") {
+        chooseName(sum, i, numb)
+        break
+      } else if ((i === 0 || i === numb - 1) && names[j].placement === "middle") {
+        chooseName(sum, i, numb)
+        break
+      } else {
+        name += names[j].name + " ";
+        
+        let chosen = names.splice(j, 1)[0];
+        used.push(chosen);
+        
+        break;
+      }
     }
-
-    let name = "";
-    const numb = Math.floor(Math.random() * 3) + 2;
-
-    for (let i = 0; i < numb; i++){
-        let sum = 0;
-        names.forEach(function(e){
-            sum += e.num;
-        });
-
-        let rand = Math.floor(Math.random() * sum);
-
-        for (let j = 0; j < names.length; j++){
-            rand -= names[j].num;
-            if (rand <= 0){
-                name += names[j].name + " ";
-
-                let chosen = names.splice(j, 1)[0];
-                used.push(chosen);
-
-                break;
-            }
-        }
-    }
-    document.getElementById("show").innerText = name;
+  }
 }
